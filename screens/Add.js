e8f9261e1cbef73fcbaa9e417c385ec6217df293
betomoedano <betomoedano@outlook.com>
2022-04-13 16:46:21 -0400

    add mutation create post

--- screens/Add.js
+++ screens/Add.js
@@ -1,23 +1,68 @@
 import * as React from 'react';
 import { SafeAreaView, Text, Button, View, TextInput, StyleSheet} from 'react-native';
+import { gql, useMutation } from '@apollo/client';
 
+const CREATE_POST = gql`
+    mutation CreatePostMutation($title: String!, $description: String!, $image: String!, $author: String!) {
+    addPost(title: $title, description: $description, image: $image, author: $author) {
+        id
+        title
+        description
+        author
+        image
+        likes
+        createdAt
+    }
+    }
+`;
 
 export default function Add() {
+
+    const [title, setTitle] = React.useState('');
+    const [description, setDescription] = React.useState('');
+    const [image, setImage] = React.useState('');
+    const [author, setAuthor] = React.useState('');
+
+    const [createPost] = useMutation(CREATE_POST, {
+        variables: {
+            title,
+            description,
+            image,
+            author
+        },
+        onCompleted: (data) => {console.log(data)},
+    });
+
+    function handlePublish() {
+        console.log(title);
+        console.log(description);
+        console.log(image);
+        console.log(author);
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput placeholder="Title" style={styles.textInput} />
+                <TextInput 
+                    onChangeText={setTitle} 
+                    placeholder="Title" style={styles.textInput} />
             </View>
             <View style={styles.inputContainer}>
-                <TextInput placeholder="Description" style={styles.textInput} />
+                <TextInput 
+                    onChangeText={setDescription}
+                    placeholder="Description" style={styles.textInput} />
             </View>
             <View style={styles.inputContainer}>
-                <TextInput placeholder="author" style={styles.textInput} />
+                <TextInput 
+                    onChangeText={setAuthor}
+                    placeholder="author" style={styles.textInput} />
             </View>
             <View style={styles.inputContainer}>
-                <TextInput placeholder="Image uri" style={styles.textInput} />
+                <TextInput 
+                    onChangeText={setImage}
+                    placeholder="Image uri" style={styles.textInput} />
             </View>
-            <Button title="Publish"/>
+            <Button title="Publish" onPress={createPost}/>
         </SafeAreaView>
     );
 }
